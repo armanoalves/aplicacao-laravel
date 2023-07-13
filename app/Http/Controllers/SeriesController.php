fb08de92d7792c1f463ecc7719e803d5c02a052c
armanoalves <armanobarrosalvesh@gmail.com>
2023-07-13 13:35:38 -0300

    Modelo base para as views

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -15,12 +15,11 @@ class SeriesController extends Controller
             'The Witcher'
       ];
   
-      $html = '<ul>';
-      foreach($series as $serie) {
-         $html .= "<li>$serie</li>";
-      }
-      $html .= '</ul>';
-  
-      return $html;
+      return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
     }
 }
